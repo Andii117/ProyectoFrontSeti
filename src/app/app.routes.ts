f946c0662e187948afc09a9eeb5cd562ec5b91ce
Harold Andres Jara <andresjara640@gmail.com>
2026-03-15 17:24:57 -0500

    Se actualiza el archivo de rutas para usar LazyLoading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,15 @@ import { Stats } from './pages/stats/stats';
 
 export const routes: Routes = [
   { path: '', component: DnaDetector },
-  { path: 'history', component: DnaHistoryComponent },
+
+  {
+    path: 'history',
+    loadComponent: () =>
+      import('./components/dna-history/dna-history').then((m) => m.DnaHistoryComponent),
+  },
   {
     path: 'stats',
-    component: Stats,
+    loadComponent: () => import('./pages/stats/stats').then((m) => m.Stats),
   },
   { path: '**', redirectTo: '' },
 ];
